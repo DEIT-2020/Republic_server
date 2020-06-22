5e7986e82d6b1c625085b01062605f350ae05df5
Jinyixuan239 <1596021045@qq.com>
2020-06-22 23:02:19 +0800

    622

--- heroes/migrations/00000004_unnamed.migration.dart
+++ heroes/migrations/00000004_unnamed.migration.dart
@@ -5,7 +5,7 @@ class Migration4 extends Migration {
   @override
   Future upgrade() async {
    		database.createTable(SchemaTable("_appUser", [SchemaColumn("id", ManagedPropertyType.bigInteger, isPrimaryKey: true, autoincrement: true, isIndexed: false, isNullable: false, isUnique: false),SchemaColumn("uname", ManagedPropertyType.string, isPrimaryKey: false, autoincrement: false, isIndexed: false, isNullable: false, isUnique: true),SchemaColumn("uemail", ManagedPropertyType.string, isPrimaryKey: false, autoincrement: false, isIndexed: false, isNullable: false, isUnique: true),SchemaColumn("upassword", ManagedPropertyType.string, isPrimaryKey: false, autoincrement: false, isIndexed: false, isNullable: false, isUnique: true)]));
-		database.deleteColumn("_Question", "checkby");
+
   }
   
   @override
