7f1b8348bd98abf7d31f6ac6e2e775d7000da61e
Jinyixuan239 <1596021045@qq.com>
2020-06-26 23:55:32 +0800

    626

--- angular/web/addQuestion.dart
+++ angular/web/addQuestion.dart
@@ -22,10 +22,13 @@ void addQuestion(Event e) async{
 var question =document.querySelector('#setQuestion');
 //window.alert((ID as InputElement).value);
 var answer=document.querySelector('#setAnswer');
+var subject=document.querySelector('#questionSubject');
 
 
-var url = 'http://localhost:8888/';
-var response = await http.post(url, body: {'question': (question as InputElement).value, 'answer':(answer as InputElement).value});//上传参数
+var url = 'http://localhost:8888/questionAdd';
+var response = await http.post(url, body: {'questionContent': (question as InputElement).value,
+'questionSubject':(subject as InputElement).value,
+ 'answer':(answer as InputElement).value});//上传参数
 print('Response status: ${response.statusCode}');
 print('Response body: ${response.body}');
 
